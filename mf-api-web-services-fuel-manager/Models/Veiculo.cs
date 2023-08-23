e45b84fefe1710709a47072e246ec5e485d3d4fc
Ramon Vicente <ramon.silva.1396106@sga.pucminas.br>
2023-08-22 21:34:36 -0300

    Cria Classe Veiculos

---
+++ mf-api-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mf_api_web_services_fuel_manager.Models
+{
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+    }
+}
